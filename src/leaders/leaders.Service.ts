894eeb3b2554c409053b794f4b81fd0d2d17d5e9
muli-shem <mulishem2002@gmail.com>
2024-10-28 15:20:04 +0300

    Feat:  Creating API

--- src/leaders/leaders.Service.ts
+++ src/leaders/leaders.Service.ts
@@ -1,2 +1,35 @@
-import {eq} from "drizzle-orm"
-impo
+import { eq } from "drizzle-orm";
+import db from "../drizzle/db";
+import { TILeaders, TSLeaders, leaders } from "../drizzle/schema";
+
+
+export const LeadersService = async (limit?:number):Promise<TSLeaders []|null> =>{
+    if (limit){
+        return await db.query.leaders.findMany({
+            limit:limit
+        })
+    }
+            return await db.query.leaders.findMany(); 
+}
+
+export const getLeadersService = async (id: number): Promise<TILeaders | undefined> => {
+    return await db.query.leaders.findFirst({
+        where: (row) => eq(row.id, id)
+    });
+};
+
+
+export const createLeadersService = async (leader: TILeaders) => {
+    await db.insert(leaders).values(leader)
+    return "User created successful"
+}
+
+export const updateLeadersService = async (id: number, leader: TILeaders)=> {
+    await db.update(leaders).set(leader).where(eq(leaders.id, id))
+    return "User updated successful"
+}
+
+export const deleteLeadersService = async (id: number) => {
+    await db.delete(leaders).where(eq(leaders.id, id))
+    return "User deleted successful"
+}
